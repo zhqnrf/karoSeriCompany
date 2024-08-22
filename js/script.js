307064a1a25d7c2b3671069cefdd811196c2b919
Zhaqian Rouf Alfauzi <zhaqianroufa@gmail.com>
2024-08-22 10:31:14 +0700

    first

---
+++ js/script.js
@@ -0,0 +1,25 @@
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
+
+var modal = document.getElementById("navModal");
+var btn = document.getElementById("menu-icon");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
